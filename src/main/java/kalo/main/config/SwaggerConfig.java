b1dd189dff049cb5ba4be256929fba916d4feb39
kalojh <121417340+kalojh@users.noreply.github.com>
2023-01-16 18:49:36 +0900

    swagger

---
+++ src/main/java/kalo/main/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package kalo.main.config;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+    
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("kalo")
+                .pathsToMatch("/**")
+                .build();
+    }
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Kalo API")
+                        .description("new Kalo 명세서입니다.")
+                        .version("v0.0.1"));
+    }
+}
